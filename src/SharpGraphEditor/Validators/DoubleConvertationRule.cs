9b1e8034b4ff56fb977128828a9bc8ca8ac4460f
Stepan Repin <stepan.repin@inbox.ru>
2017-04-03 18:54:15 +0300

    Add validation in textboxes in VertexPropertiesView

---
+++ src/SharpGraphEditor/Validators/DoubleConvertationRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Controls;
+
+namespace SharpGraphEditor.Validators
+{
+    public class DoubleConvertationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var valueStr = value.ToString();
+            if (valueStr.Length > 0)
+            {
+                if (Double.TryParse(valueStr, out double res))
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+            return new ValidationResult(false, "Input must be rational number (max 15 symbols)");
+        }
+    }
+}
