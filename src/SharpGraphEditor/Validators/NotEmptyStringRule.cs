cd2ff9f6c8c990e7aebc1ad691693afa32709934
Stepan Repin <stepan.repin@inbox.ru>
2017-04-03 21:20:11 +0300

    Add Name property to VertexPropertiesView

---
+++ src/SharpGraphEditor/Validators/NotEmptyStringRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Controls;
+
+namespace SharpGraphEditor.Validators
+{
+    public class NotEmptyStringRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (String.IsNullOrWhiteSpace(value.ToString()))
+            {
+                return new ValidationResult(false, "Input string must be not empty");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}
