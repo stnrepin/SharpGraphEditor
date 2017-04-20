bd8cda39537bb2716b78e0ad9c4bce9cbaba6c2a
Stepan Repin <stepan.repin@inbox.ru>
2017-04-20 19:35:08 +0300

    Fix bug: incorrectly determines the center of the top

--- src/SharpGraphEditor/Controls/GraphElements/VertexControl.xaml.cs
+++ src/SharpGraphEditor/Controls/GraphElements/VertexControl.xaml.cs
@@ -10,6 +10,12 @@ namespace SharpGraphEditor.Controls.GraphElements
         public VertexControl()
         {
             InitializeComponent();
+            SizeChanged += (_, __) =>
+            {
+                var centerX = ActualWidth / 2;
+                var centerY = ActualHeight / 2;
+                Margin = new System.Windows.Thickness(-centerX, -centerY, centerX, centerY);
+            };
         }
     }
 }
